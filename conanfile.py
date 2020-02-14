731b42e2153cb05156784ad66d5ee7400445a0a5
Brede Johansen <brede.johansen@km.kongsberg.com>
2020-02-14 17:19:07 +0100

    Update to HighFive version 2.1.1

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class HighFiveConan(ConanFile):
     name = "HighFive"
-    version = "2.0"
+    version = "2.1.1"
     description = "Header-only C++ HDF5 interface"
     topics = ("conan", "HighFive", "HDF5")
     url = "https://github.com/bredej/conan-highfive"
@@ -13,12 +13,12 @@ class HighFiveConan(ConanFile):
     license = "BSL-1.0"
     no_copy_source = True
     exports = ["LICENSE"]
-    requires = "hdf5/1.10.5-dm1@ess-dmsc/stable"
+    requires = "hdf5/1.10.6@_/_"
     _source_subfolder = "source_subfolder"
 
     def source(self):
         source_url = "https://github.com/BlueBrain/HighFive"
-        tools.get("{0}/archive/v{1}.zip".format(source_url, self.version), sha256="35d594207ce7971d257aafe92c0299dd9c1e749e244a39f0a6479818c96087a1")
+        tools.get("{0}/archive/v{1}.zip".format(source_url, self.version), sha256="F08624AFB1F3F28DDEAB60A63A27B592DC6223294EB0324F95D47D2F8A93926E")
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
